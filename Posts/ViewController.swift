76e6f5d561f9f904293be0ba60f74432fc39afe9
agustiyan <agustiyaninfo@gmail.com>
2022-07-29 20:18:45 +0700

    Initial Commit

---
+++ Posts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Agus Tiyansyah Syam on 29/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
